f618e7b4696df624d234d656104e670c1aad53ec
Mattias Nyberg <me@cp0la.se>
2019-01-10 21:43:29 +0100

    Added automatic creation of root folder for download

--- src/download.rb
+++ src/download.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 if (ARGV.include?("--h") || ARGV.include?("-help"))
 	puts "Image downloader for personverser @ UUB"
 	puts " Usage: ruby download.rb [count|109853] [offset|1] [output_root_dir|./verser]"
@@ -16,9 +18,10 @@ end
 
 offset = ARGV.length > 1 ? ARGV[1].to_i(10) : 1
 count = ARGV.length > 0 ? ARGV[0].to_i(10) : 108953
-output_root = ARGV.length > 2 ? ARGV[2] : "./verser"
+outpath_root = ARGV.length > 2 ? ARGV[2] : "./verser"
 
-outpath_gen = lambda {|num| "#{output_root}/#{num}.jpg"}
+outpath_gen = lambda {|num| "#{outpath_root}/#{num}.jpg"}
 address_gen = lambda {|num| "https://deva.its.uu.se/upps-personverser/PictureLoader?Antialias=ON&ImageId=#{num}&Scale=1"}
 
+FileUtils.mkdir_p(outpath_root)
 download_entries(offset, count, outpath_gen, address_gen)
